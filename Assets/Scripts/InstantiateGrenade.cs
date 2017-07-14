dc0d35697232a27332f036de383cf7830bf93973
Mark <joya.mark@gmail.com>
2017-07-15 03:43:15 +1000

    Fixed grenade respawning bug

--- Assets/Scripts/InstantiateGrenade.cs
+++ Assets/Scripts/InstantiateGrenade.cs
@@ -5,25 +5,47 @@ using UnityEngine;
 public class InstantiateGrenade : MonoBehaviour {
 
 	public GameObject grenadePrefab;
+	public int SPAWN_LIMIT;
+
+	private bool isOccupied;
+	private int spawnedObjects = 0;
+
+	private float currentTime;
+	private float lastSpawnTime;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		CreateGrenade();
+	}
+
+	void Update()
+	{
+		if (!isOccupied) CreateGrenade();
 	}
 
 	public void CreateGrenade()
 	{
-		Instantiate(grenadePrefab, this.transform.position, this.transform.rotation);
-	
+		if (spawnedObjects < SPAWN_LIMIT)
+		{
+			Instantiate(grenadePrefab, this.transform.position, this.transform.rotation);
+			spawnedObjects++;
+		}
 	}
 	
 	public void OnTriggerExit(Collider other)
 	{
-		Debug.Log("EXIT");
-		if (other.CompareTag("Grenade"))
-		{
-			//CreateGrenade();
-		}
+		isOccupied = false;
 	}
 
+	public void OnTriggerEntry(Collider other)
+	{
+		isOccupied = true;
+	}
+
+	public void OnTriggerStay(Collider other)
+	{
+		isOccupied = true;
+	}
+	
 }
