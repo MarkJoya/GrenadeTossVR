e20fd7bee76d543e00ae896a72d6cfdfd1c6705e
Mark <joya.mark@gmail.com>
2017-07-16 02:34:08 +1000

    Added some missing scripts and prefabs

---
+++ Assets/Scripts/GrenadeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeScript : MonoBehaviour {
+
+	private bool timerSet;
+
+	// Use this for initialization
+	void Start ()
+	{
+		timerSet = false;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	// Sets timer to indicate that this grenade's explosion timer is ticking
+	public void SetTimer()
+	{
+		timerSet = true;
+	}
+
+	public bool IsTimerSet()
+	{
+		return timerSet;
+	}
+}
