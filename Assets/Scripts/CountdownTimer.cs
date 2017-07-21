b50a3cca05139e0f1acbd32bf0784cb2c1a47581
Mark <joya.mark@gmail.com>
2017-07-21 22:40:07 +1000

    Added extra instruction text at game end

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -7,7 +7,7 @@ public class CountdownTimer : MonoBehaviour {
 	public GameObject scoreTextObject;
 
 	private static double PRE_TIME = 3;
-	private static double GAME_TIME = 20;
+	private static double GAME_TIME = 7;
 
 	private double preTimeLeft = PRE_TIME;
 	private double timeLeft = GAME_TIME;
@@ -42,9 +42,9 @@ public class CountdownTimer : MonoBehaviour {
 		//When time runs out, show final score
 		else
 		{
-			txtRef.fontSize = 5;
+			txtRef.fontSize = 2;
 			int score = scoreTextObject.GetComponent<ScoreBoard>().GetScore();
-			txtRef.text = "Score: " + score.ToString();
+			txtRef.text = "Score: " + score.ToString() + "\n" + "Squeeze yellow box to restart";
 		}
 	}
 
